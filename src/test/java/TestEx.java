3047dab88b04186c879a3696bf9b0eacdacc43c5
shane.miller <shane.michael.miller@gmail.com>
2018-08-31 05:02:13 -0500

    Test VSTS Test Issue

---
+++ src/test/java/TestEx.java
@@ -0,0 +1,56 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/* Execute 10 unit tests */
+public class TestEx {
+
+    @Test
+    public void test0() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test5() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test6() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test7() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test8() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test9() {
+        Assert.assertTrue(true);
+    }
+}
